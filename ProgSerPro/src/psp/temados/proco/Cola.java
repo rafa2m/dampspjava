d71c8bcc55d1f2e60b0f2b91798c37f7e44c324c
rafa2m <rafaelmendez@iesantoniogala.es>
2022-01-27 08:49:42 +0100

    Se corrije la posición de los mensajes de consola en get() y put()

--- ProgSerPro/src/psp/temados/proco/Cola.java
+++ ProgSerPro/src/psp/temados/proco/Cola.java
@@ -14,13 +14,14 @@ public class Cola {
 				wait();
 			}catch(InterruptedException e){}
 		}
+
+		notify();
+		this.disponible = false;
 		//este es el mensaje que estaba en el consumidor
 		//, ahora esta línea esta sincronizada y se ejecuta
 		//justo antes de consumir el valor de la cola
 		System.out.println(i + "=>Consumidor: " + 
 				hilo + ", consume: " + this.numero);
-		notify();
-		this.disponible = false;
 		return this.numero;
 	}
 	public synchronized void put(int valor, int hilo){
@@ -28,14 +29,14 @@ public class Cola {
 			try{
 				wait();
 			}catch(InterruptedException e){}
-		}
+		}		
+		this.numero = valor;
+		notify();
+		this.disponible = true;
 		//este es el mensaje que estaba en el consumidor
 		//, ahora esta línea esta sincronizada y se ejecuta
 		//justo antes de consumir el valor de la cola
 		System.out.println(valor + "=>Productor : " 
 				+ hilo	+ ", produce: " + this.numero);
-		this.numero = valor;
-		notify();
-		this.disponible = true;
 	}
 }
