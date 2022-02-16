c61caf5212297d53118de522ea8711db55f2754a
rafa2m <rafaelmendez@iesantoniogala.es>
2022-02-16 14:26:51 +0100

    Ejemplo de cliente FTP

--- ProyectoServiciosRed/src/main/java/dam/servicios/red/FtpConnectAndLoginDemo.java
+++ ProyectoServiciosRed/src/main/java/dam/servicios/red/FtpConnectAndLoginDemo.java
@@ -9,17 +9,23 @@ import org.apache.commons.net.ftp.FTPReply;
 public class FtpConnectAndLoginDemo {	
 	public static void main(String[] args) {
 		//se almacena el nombre del servidor FTP
-		String server = "ftp.rediris.es";
+		String server = "137.74.235.45";
 		//indicamos el puerto TCP
 		int port = 21;
 		//guardamos usuario y clave
-		String user = "anonymous";
-		String pass = "anonymous";
+		String user = "dam2psp@iesantoniogala.es";
+		String pass = "GalaDamPsp_2022";
 		//instanciamos objeto de FTPClient
 		FTPClient ftpClient = new FTPClient();
 		try {
 			//conectamos con el servidor
 			ftpClient.connect(server, port);
+			//se delimitan los puertos utilizables
+			//en el lado del cliente
+			ftpClient.setActivePortRange(2000, 3500);
+			//trabajamos en modo pasivo, para transferencia
+			//de datos entre cliente y servidor
+			ftpClient.enterLocalPassiveMode();
 			
 			showServerReply(ftpClient);
 			int replyCode = ftpClient.getReplyCode();
@@ -36,12 +42,13 @@ public class FtpConnectAndLoginDemo {
 			} else {
 				System.out.println("LOGGED IN SERVER");
 			}
+			
 			//cambiamos de directorio
-			boolean result = ftpClient.changeWorkingDirectory("/mirror");
+			boolean result = ftpClient.changeWorkingDirectory("/dampsp");
 			showServerReply(ftpClient);
 			if(result) {
 				//listamos todos los ficheros del directorio /mirror
-				FTPFile[] files = ftpClient.listFiles("/mirror");
+				FTPFile[] files = ftpClient.listFiles("/dampsp");
 				showServerReply(ftpClient);
 				for(FTPFile file:files) {
 					System.out.println(file.getName());
