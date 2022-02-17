22a042096f1da14dbb063705a967a54ef7034eb7
rafa2m <rafaelmendez@iesantoniogala.es>
2022-02-17 09:53:06 +0100

    Ejemplo de cliente/servidor sockets con objetos

--- ProgSerPro/src/psp/tematres/sockets/TcpClient.java
+++ ProgSerPro/src/psp/tematres/sockets/TcpClient.java
@@ -3,6 +3,9 @@ package psp.tematres.sockets;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 /**
  * Ejemplo de cliente socket. Se envía un mensaje al servidor y se espera la respuesta
@@ -12,30 +15,51 @@ import java.net.Socket;
  */
 public class TcpClient {
 	public static void main(String argv[]) throws Exception {
+		Message message;
 		String sentence;
 		String modifiedSentence;
+		
+		message = new Message("Rafa",
+				InetAddress.getLocalHost().getHostAddress());
+		
+		
 		//se abre el flujo de lectura desde la consola
 		BufferedReader inFromUser = new BufferedReader( new 
 				InputStreamReader(System.in));
+		
+		
 		//se conecta con el servidor
 		Socket clientSocket = new Socket("localhost", 6789);
+		
+		//asigno mi puerto TCP al mensaje
+		message.setPort(clientSocket.getLocalPort());
+		
 		//se obtiene el flujo de escritura del servidor para
 		//hablar con él
-		DataOutputStream outToServer = new 
-				DataOutputStream(clientSocket.
+		ObjectOutputStream outToServer = new 
+				ObjectOutputStream(clientSocket.
 						getOutputStream());
 		//se obtiene el flujo de lectura para escuchar al servidor
-		BufferedReader inFromServer = new BufferedReader(new 
-				InputStreamReader(clientSocket.
-						getInputStream()));
+		ObjectInputStream inFromServer = new 
+				ObjectInputStream(clientSocket.
+						getInputStream());
+		
 		//se lee de consola la entrada del usuario
 		sentence = inFromUser.readLine();
+		
+		//actualizo el texto del mensaje
+		message.setMessage(sentence);
+		
 		//se envía lo que escribe el usuario al servidor
-		outToServer.writeBytes(sentence + "\n");
+		outToServer.writeObject(message);
+		//outToServer.writeBytes(sentence + "\n");
+		
 		//se espera la respuesta del servidor
-		modifiedSentence = inFromServer.readLine();
+		message = (Message)inFromServer.readObject();
+		//modifiedSentence = inFromServer.readLine();
+		
 		//el servidor ha respondido, se muestra la respuesta en consola
-		System.out.println("FROM SERVER: " + modifiedSentence);
+		System.out.println("FROM SERVER: " + message.getMessage());
 		//se cierra el socket
 		clientSocket.close();
 	}
--- ProgSerPro/src/psp/tematres/sockets/TcpServer.java
+++ ProgSerPro/src/psp/tematres/sockets/TcpServer.java
@@ -3,6 +3,8 @@ package psp.tematres.sockets;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 /**
@@ -19,6 +21,7 @@ public class TcpServer {
 	public static void main(String argv[]) throws Exception {
 		String clientSentence;
 		String capitalizedSentence;
+		Message message;
 		ServerSocket welcomeSocket = new ServerSocket(6789);
 
 		while(true) {
@@ -26,24 +29,26 @@ public class TcpServer {
 			Socket connectionSocket = welcomeSocket.accept();
 			//se ha recibido una conexión del lado del cliente
 			//se obtiene el flujo de lectura para escuchar al cliente
-			BufferedReader inFromClient =
-					new BufferedReader(new 
-							InputStreamReader(connectionSocket.
-									getInputStream()));
+			ObjectInputStream inFromClient =
+					new ObjectInputStream(connectionSocket.
+									getInputStream());
 			//se obtiene el flujo de escritura para hablar con el 
 			//cliente
-			DataOutputStream outToClient = new 
-					DataOutputStream(connectionSocket.
+			ObjectOutputStream outToClient = new 
+					ObjectOutputStream(connectionSocket.
 							getOutputStream());
 			//se escucha al cliente
-			clientSentence = inFromClient.readLine();
+			//clientSentence = inFromClient.readLine();
+			message = (Message)inFromClient.readObject();
+			
 			//el cliente ha hablado y la información se convierte a mayúsculas
 			//y se responde al cliente
-			System.out.println("Received: " + clientSentence);
-			capitalizedSentence = clientSentence.toUpperCase() + 
+			System.out.println("Received: " + message.getMessage());
+			capitalizedSentence = message.getMessage().toUpperCase() + 
 					"\n";
 			//envío de la respuesta al cliente
-			outToClient.writeBytes(capitalizedSentence);
+			//outToClient.writeBytes(capitalizedSentence);
+			outToClient.writeObject(message);
 		}
 	}
 }
